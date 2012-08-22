b370c5423d1ba7a8e8760ed06271ab80e1d1ac2b
Patrick Campanale <patrickappcreator@gmail.com>
2012-08-22 00:02:17 -0500

    changed the summary of the main page. instructions on how to set up a local server coming soon

--- source/App.js
+++ source/App.js
@@ -6,32 +6,7 @@ enyo.kind({
 		
 		{kind: "Scroller", fit: true, style: "height:300px;", components: [
 		
-			{tag: "hr", content: "Step 1"},
-			
-	    	{kind: "onyx.Button", name: "btnRequestAuth", content: "Request Authorization", ontap: "requestAuth"},
-	    	{style: "height:40px;"},
-	    	
-	    	{tag: "hr", content: "Step 2"},
-	    	{kind: "onyx.Groupbox", content: "PIN", components: [
-	    		{kind: "Input", name: "txtPin", components: [
-	    			{kind: "onyx.Button", name: "btnValidatePin", content: "Validate PIN", ontap: "validatePin", onmousedown: "dontFocus", disabled: true}
-	    		]}
-	    	]},
-	    	{style: "height:40px;"},
-	    	
-	    	{tag: "hr", content: "Step 3"},
-	    	{kind: "onyx.Groupbox", content: "Tweet", components: [
-	    		{kind: "Input", name: "tweetText", components: [
-	    			{kind: "onyx.Button", name: "btnTweetText", content: "Tweet Text", ontap: "tweetText", onmounedown: "dontFocus", disabled: true},
-	    			]}
-	    	]},
-	    	{style: "height:40px"},
-	    	
-	    	{tag: "hr", content: "Step 4"},
-	    	{kind: "onyx.Button", name: "btnFetchTimeline", content: "Fetch home timeline data", ontap: "fetchTimeline", disabled: false},
-    		{style: "height:40px;"},
-    		
-    		{name: "myContent"}
+			{content: "This application is still in the alpha stages for both design and usage. This page is only to test to make sure you have configured Chrome to run Enyo correctly from a local server."}
     	]}
 	],
 	create: function() {
